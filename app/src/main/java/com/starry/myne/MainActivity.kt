0abb3ce004c2bffdd3463ce78d05e0324ddc3ed1
Stɑrry Shivɑm <krshivam24@proton.me>
2023-01-12 11:11:18 +0530

    Add swipe actions in library (#9)
    * Improve about screen
    
    Signed-off-by: starry69 <krshivam24@proton.me>
    
    * Add swipe actions in library items
    >>
    >>
    >> - Swipe right to delete the ebook file
    >> - Swipe left yo share the ebook file
    >>
    
    Signed-off-by: starry69 <krshivam24@proton.me>
    
    Signed-off-by: starry69 <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/MainActivity.kt
+++ app/src/main/java/com/starry/myne/MainActivity.kt
@@ -25,12 +25,15 @@ import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import coil.annotation.ExperimentalCoilApi
@@ -39,7 +42,7 @@ import com.starry.myne.others.NetworkObserver
 import com.starry.myne.ui.screens.MainScreen
 import com.starry.myne.ui.theme.MyneTheme
 import com.starry.myne.ui.viewmodels.ThemeMode
-import com.starry.myne.ui.viewmodels.ThemeViewModel
+import com.starry.myne.ui.viewmodels.SettingsViewModel
 import com.starry.myne.utils.PreferenceUtils
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -51,29 +54,29 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var networkObserver: NetworkObserver
-    lateinit var themeViewModel: ThemeViewModel
+    lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         PreferenceUtils.initialize(this)
         networkObserver = NetworkObserver(applicationContext)
-        themeViewModel = ViewModelProvider(this)[ThemeViewModel::class.java]
+        settingsViewModel = ViewModelProvider(this)[SettingsViewModel::class.java]
 
         when (PreferenceUtils.getInt(PreferenceUtils.APP_THEME, ThemeMode.Auto.ordinal)) {
-            ThemeMode.Auto.ordinal -> themeViewModel.setTheme(ThemeMode.Auto)
-            ThemeMode.Dark.ordinal -> themeViewModel.setTheme(ThemeMode.Dark)
-            ThemeMode.Light.ordinal -> themeViewModel.setTheme(ThemeMode.Light)
+            ThemeMode.Auto.ordinal -> settingsViewModel.setTheme(ThemeMode.Auto)
+            ThemeMode.Dark.ordinal -> settingsViewModel.setTheme(ThemeMode.Dark)
+            ThemeMode.Light.ordinal -> settingsViewModel.setTheme(ThemeMode.Light)
         }
 
-        themeViewModel.setMaterialYou(
+        settingsViewModel.setMaterialYou(
             PreferenceUtils.getBoolean(
                 PreferenceUtils.MATERIAL_YOU, Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
             )
         )
 
         setContent {
-            MyneTheme(themeViewModel = themeViewModel) {
+            MyneTheme(settingsViewModel = settingsViewModel) {
 
                 val systemUiController = rememberSystemUiController()
                 systemUiController.setSystemBarsColor(
@@ -83,7 +86,13 @@ class MainActivity : AppCompatActivity() {
                 val status by networkObserver.observe().collectAsState(
                     initial = NetworkObserver.Status.Unavailable
                 )
-                MainScreen(status)
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(status)
+                }
             }
         }
         checkStoragePermission()
--- app/src/main/java/com/starry/myne/ui/viewmodels/BookDetailViewModel.kt
+++ app/src/main/java/com/starry/myne/ui/viewmodels/BookDetailViewModel.kt
@@ -80,7 +80,7 @@ class BookDetailViewModel @Inject constructor(private val libraryDao: LibraryDao
             val manager = activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
             val uri = Uri.parse(book.formats.applicationepubzip)
             val request = DownloadManager.Request(uri)
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
                 .setAllowedOverRoaming(true)
                 .setAllowedOverMetered(true)
                 .setTitle(book.title)
