d2b673c2e5e72421a7c5a4b73fe3cea818a4f8a8
starry-shivam <krshivam24@proton.me>
2023-03-18 20:37:55 +0530

    change default color schema
    and add missing copyright headers
    
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/di/MainModule.kt
+++ app/src/main/java/com/starry/myne/di/MainModule.kt
@@ -1,5 +1,3 @@
-package com.starry.myne.di
-
 /*
 Copyright 2022 - 2023 Stɑrry Shivɑm
 
@@ -15,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+
+package com.starry.myne.di
 
 import android.content.Context
 import com.starry.myne.database.MyneDatabase
--- app/src/main/java/com/starry/myne/ui/screens/welcome/composables/WelcomeScreen.kt
+++ app/src/main/java/com/starry/myne/ui/screens/welcome/composables/WelcomeScreen.kt
@@ -1,3 +1,20 @@
+/*
+Copyright 2022 - 2023 Stɑrry Shivɑm
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 package com.starry.myne.ui.screens.welcome.composables
 
 import androidx.compose.foundation.background
@@ -90,7 +107,6 @@ fun WelcomeScreen(navController: NavController) {
                     .height(50.dp)
                     .fillMaxWidth(0.8f),
                 shape = RoundedCornerShape(12.dp),
-                //colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
             ) {
                 Text(
                     text = internalReaderValue,
