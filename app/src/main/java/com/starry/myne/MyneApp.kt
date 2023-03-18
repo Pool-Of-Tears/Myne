b69e8c95ddd6d56f35e9f2574120a4ed6a638b58
starry-shivam <krshivam24@proton.me>
2023-03-18 17:51:05 +0530

    Add a crash handler
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/MyneApp.kt
+++ app/src/main/java/com/starry/myne/MyneApp.kt
@@ -17,7 +17,22 @@ limitations under the License.
 package com.starry.myne
 
 import android.app.Application
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.ExperimentalComposeUiApi
+import cat.ereza.customactivityoncrash.config.CaocConfig
+import coil.annotation.ExperimentalCoilApi
 import dagger.hilt.android.HiltAndroidApp
 
+
+@ExperimentalCoilApi
+@ExperimentalComposeUiApi
+@ExperimentalMaterial3Api
+@ExperimentalMaterialApi
 @HiltAndroidApp
-class MyneApp : Application()
+class MyneApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CaocConfig.Builder.create().restartActivity(MainActivity::class.java).apply()
+    }
+}
