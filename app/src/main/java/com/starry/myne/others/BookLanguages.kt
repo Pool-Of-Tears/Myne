d3f124108e84ea37774b33116e73d498b7e1dd91
starry-shivam <krshivam24@proton.me>
2023-03-24 12:46:45 +0530

    Avoid R8 from deleting language classes
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/others/BookLanguages.kt
+++ app/src/main/java/com/starry/myne/others/BookLanguages.kt
@@ -1,5 +1,8 @@
 package com.starry.myne.others
 
+import androidx.annotation.Keep
+
+@Keep
 sealed class BookLanguages(val name: String, val isoCode: String) {
 
     companion object {
@@ -7,18 +10,45 @@ sealed class BookLanguages(val name: String, val isoCode: String) {
             BookLanguages::class.sealedSubclasses.mapNotNull { it.objectInstance }
     }
 
-    object AllBooks: BookLanguages("All Books","all")
+    @Keep
+    object AllBooks : BookLanguages("All Books", "all")
+
+    @Keep
     object Chinese : BookLanguages("Chinese", "zh")
+
+    @Keep
     object Danish : BookLanguages("Danish", "da")
+
+    @Keep
     object Dutch : BookLanguages("Dutch", "nl")
+
+    @Keep
     object English : BookLanguages("English", "en")
+
+    @Keep
     object Finnish : BookLanguages("Finnish", "fi")
+
+    @Keep
     object French : BookLanguages("French", "fr")
+
+    @Keep
     object German : BookLanguages("German", "de")
+
+    @Keep
     object Hungarian : BookLanguages("Hungarian", "hu")
+
+    @Keep
     object Italian : BookLanguages("Italian", "it")
+
+    @Keep
     object Portuguese : BookLanguages("Portuguese", "pt")
-    object Russian: BookLanguages("Russian","ru")
+
+    @Keep
+    object Russian : BookLanguages("Russian", "ru")
+
+    @Keep
     object Spanish : BookLanguages("Spanish", "es")
+
+    @Keep
     object Swedish : BookLanguages("Swedish", "sv")
 }
