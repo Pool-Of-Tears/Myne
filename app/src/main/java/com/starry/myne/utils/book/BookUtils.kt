503b9c47e8988d0cd80100e77267499e9a698c44
starry-shivam <krshivam24@proton.me>
2024-04-14 11:27:19 +0530

    Add Kdocs
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
+++ app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
@@ -20,6 +20,13 @@ import com.starry.myne.repo.models.Author
 import java.util.Locale
 
 object BookUtils {
+
+    /**
+     * Converts the list of authors into a single string.
+     *
+     * @param authors List of authors.
+     * @return String representation of the authors.
+     */
     fun getAuthorsAsString(authors: List<Author>): String {
         return if (authors.isEmpty()) {
             "Unknown Author"
@@ -28,14 +35,36 @@ object BookUtils {
         }
     }
 
+    /**
+     * For some weird reasons, gutenberg gives name of authors in
+     * reversed, where first name and last are separated by a comma
+     * Eg: "Fyodor Dostoyevsky" becomes "Dostoyevsky, Fyodor", This
+     * function fixes that and returns name in correct format.
+     *
+     * @param name Name of the author.
+     * @return Name of the author in correct format.
+     */
     private fun fixAuthorName(name: String): String {
         return name.split(",").reversed().joinToString(" ") { it.trim() }
     }
 
+    /**
+     * Converts the list of languages into a single string.
+     *
+     * @param languages List of languages.
+     * @return String representation of the languages.
+     */
     fun getLanguagesAsString(languages: List<String>): String {
         return languages.joinToString(", ") { Locale(it).displayLanguage }
     }
 
+    /**
+     * Converts the list of subjects into a single string.
+     *
+     * @param subjects List of subjects.
+     * @param limit Maximum number of subjects to show.
+     * @return String representation of the subjects.
+     */
     fun getSubjectsAsString(subjects: List<String>, limit: Int): String {
         val allSubjects = subjects.flatMap { it.split("--") }.map { it.trim() }.toSet()
         val truncatedSubs = if (allSubjects.size > limit) allSubjects.take(limit) else allSubjects
