32b557ffe4238a10c263a6847d8e1e7f00a4653f
starry-shivam <krshivam24@proton.me>
2024-04-14 11:22:55 +0530

    Optimize bookutils
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
+++ app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
@@ -21,66 +21,24 @@ import java.util.Locale
 
 object BookUtils {
     fun getAuthorsAsString(authors: List<Author>): String {
-        return if (authors.isNotEmpty()) {
-            var result: String
-            if (authors.size > 1) {
-                result = fixAuthorName(authors.first().name)
-                authors.slice(1 until authors.size).forEach { author ->
-                    if (author.name != "N/A")
-                        result += ", ${fixAuthorName(author.name)}"
-                }
-            } else {
-                result = fixAuthorName(authors.first().name)
-            }
-            result
-        } else {
+        return if (authors.isEmpty()) {
             "Unknown Author"
+        } else {
+            authors.joinToString(", ") { fixAuthorName(it.name) }
         }
     }
 
-    /**
-     * For some weird reasons, gutenberg gives name of authors in
-     * reversed, where first name and last are separated by a comma
-     * Eg: "Fyodor Dostoyevsky" becomes "Dostoyevsky, Fyodor", This
-     * function fixes that and returns name in correct format.
-     */
     private fun fixAuthorName(name: String): String {
-        val reversed = name.split(",").reversed()
-        return reversed.joinToString(separator = " ") {
-            return@joinToString it.trim()
-        }
+        return name.split(",").reversed().joinToString(" ") { it.trim() }
     }
 
     fun getLanguagesAsString(languages: List<String>): String {
-        var result = ""
-        languages.forEachIndexed { index, lang ->
-            val loc = Locale(lang)
-            if (index == 0) {
-                result = loc.displayLanguage
-            } else {
-                result += ", ${loc.displayLanguage}"
-            }
-        }
-        return result
+        return languages.joinToString(", ") { Locale(it).displayLanguage }
     }
 
     fun getSubjectsAsString(subjects: List<String>, limit: Int): String {
-        val allSubjects = ArrayList<String>()
-        // strip "--" from subjects.
-        subjects.forEach { subject ->
-            if (subject.contains("--")) {
-                allSubjects.addAll(subject.split("--"))
-            } else {
-                allSubjects.add(subject)
-            }
-        }
-        val truncatedSubs: List<String> = if (allSubjects.size > limit) {
-            allSubjects.toSet().toList().subList(0, limit)
-        } else {
-            allSubjects.toSet().toList()
-        }
-        return truncatedSubs.joinToString(separator = ", ") {
-            return@joinToString it.trim()
-        }
+        val allSubjects = subjects.flatMap { it.split("--") }.map { it.trim() }.toSet()
+        val truncatedSubs = if (allSubjects.size > limit) allSubjects.take(limit) else allSubjects
+        return truncatedSubs.joinToString(", ")
     }
 }
