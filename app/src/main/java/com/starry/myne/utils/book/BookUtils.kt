2c8218486122ea7a43613b57815a6bc74c63ce56
Stɑrry Shivɑm <krshivam24@proton.me>
2024-04-14 11:34:58 +0530

    Optimize bookutils (#149)

--- app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
+++ app/src/main/java/com/starry/myne/utils/book/BookUtils.kt
@@ -20,21 +20,18 @@ import com.starry.myne.repo.models.Author
 import java.util.Locale
 
 object BookUtils {
+
+    /**
+     * Converts the list of authors into a single string.
+     *
+     * @param authors List of authors.
+     * @return String representation of the authors.
+     */
     fun getAuthorsAsString(authors: List<Author>): String {
-        return if (authors.isNotEmpty()) {
-            var result: String
-            if (authors.size > 1) {
-                result = fixAuthorName(authors.first().name)
-                authors.slice(1 until authors.size).forEach { author ->
-                    if (author.name != "N/A")
-                        result += ", ${fixAuthorName(author.name)}"
-                }
-            } else {
-                result = fixAuthorName(authors.first().name)
-            }
-            result
-        } else {
+        return if (authors.isEmpty()) {
             "Unknown Author"
+        } else {
+            authors.joinToString(", ") { fixAuthorName(it.name) }
         }
     }
 
@@ -43,44 +40,34 @@ object BookUtils {
      * reversed, where first name and last are separated by a comma
      * Eg: "Fyodor Dostoyevsky" becomes "Dostoyevsky, Fyodor", This
      * function fixes that and returns name in correct format.
+     *
+     * @param name Name of the author.
+     * @return Name of the author in correct format.
      */
     private fun fixAuthorName(name: String): String {
-        val reversed = name.split(",").reversed()
-        return reversed.joinToString(separator = " ") {
-            return@joinToString it.trim()
-        }
+        return name.split(",").reversed().joinToString(" ") { it.trim() }
     }
 
+    /**
+     * Converts the list of languages into a single string.
+     *
+     * @param languages List of languages.
+     * @return String representation of the languages.
+     */
     fun getLanguagesAsString(languages: List<String>): String {
-        var result = ""
-        languages.forEachIndexed { index, lang ->
-            val loc = Locale(lang)
-            if (index == 0) {
-                result = loc.displayLanguage
-            } else {
-                result += ", ${loc.displayLanguage}"
-            }
-        }
-        return result
+        return languages.joinToString(", ") { Locale(it).displayLanguage }
     }
 
+    /**
+     * Converts the list of subjects into a single string.
+     *
+     * @param subjects List of subjects.
+     * @param limit Maximum number of subjects to show.
+     * @return String representation of the subjects.
+     */
     fun getSubjectsAsString(subjects: List<String>, limit: Int): String {
-        val allSubjects = ArrayList<String>()
-        // strip "--" from subjects.
-        subjects.forEach { subject ->
-            if (subject.contains("--")) {
-                allSubjects.addAll(subject.split("--"))
-            } else {
-                allSubjects.add(subject)
-            }
-        }
-        val truncatedSubs: List<String> = if (allSubjects.size > limit) {
-            allSubjects.toSet().toList().subList(0, limit)
-        } else {
-            allSubjects.toSet().toList()
-        }
-        return truncatedSubs.joinToString(separator = ", ") {
-            return@joinToString it.trim()
-        }
+        val allSubjects = subjects.flatMap { it.split("--") }.map { it.trim() }.toSet()
+        val truncatedSubs = if (allSubjects.size > limit) allSubjects.take(limit) else allSubjects
+        return truncatedSubs.joinToString(", ")
     }
 }
